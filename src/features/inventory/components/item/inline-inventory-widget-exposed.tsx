1c6bde794041787df08bcf336f0798aa1f63f3fc
khinemyaezin <khinemyaezin@icloud.com>
2026-09-07 12:56:13 +0700

    refactor: migrate useInventoryNewSlot to SlotHandle interface and unify state management

--- src/features/inventory/components/item/inline-inventory-widget-exposed.tsx
+++ src/features/inventory/components/item/inline-inventory-widget-exposed.tsx
@@ -2,37 +2,56 @@ import { EntryLinkProvider, PlatformProvider } from "@khinemyaezin/seller-ui";
 import { type HateoasLink } from "@khinemyaezin/seller-api";
 import {
   InventoryCreateContext,
+  InventoryPayload,
   PRODUCT_EXTENSION_SLOTS,
   type ExtensionMountProps,
   type SellerPlatform,
 } from "@khinemyaezin/seller-contracts";
 import InlineInventoryWidget from "./inline-inventory-widget";
-import useInventoryNewSlot from "../../hooks/use-inventory-new-slot";
+import useInventoryNewSlot, {
+  type UseInventoryNewSlotProps,
+} from "../../hooks/use-inventory-new-slot";
 
 export type InlineInventoryWidgetExposedProps = ExtensionMountProps & {
   entryLink: HateoasLink;
   platform?: SellerPlatform;
 };
 
+function InlineInventoryWidgetBound(props: UseInventoryNewSlotProps) {
+  const { context, payload, ref, onChange } = useInventoryNewSlot(props);
+
+  return (
+    <InlineInventoryWidget
+      ref={ref}
+      value={payload}
+      context={context}
+      onChange={onChange}
+    />
+  );
+}
 
 export default function InlineInventoryWidgetExposed({
   groupId,
   slotId = PRODUCT_EXTENSION_SLOTS.CREATE_INVENTORY_INLINE,
-  context: initialContext,
+  context,
+  initialValue,
+  onChange,
+  registerHandle,
   platform,
   entryLink,
 }: InlineInventoryWidgetExposedProps) {
-  const { context, payload, ref, onChange } = useInventoryNewSlot({ groupId, slotId, platform, initialContext: initialContext as InventoryCreateContext });
   if (!entryLink || !groupId) return null;
 
   return (
     <PlatformProvider platform={platform}>
       <EntryLinkProvider link={entryLink}>
-        <InlineInventoryWidget
-          ref={ref}
-          value={payload}
-          context={context}
-          onChange={onChange}
+        <InlineInventoryWidgetBound
+          groupId={groupId}
+          slotId={slotId}
+          context={context as InventoryCreateContext | undefined}
+          initialValue={initialValue as InventoryPayload | undefined}
+          onChange={onChange as ((value: InventoryPayload) => void) | undefined}
+          registerHandle={registerHandle}
         />
       </EntryLinkProvider>
     </PlatformProvider>
--- src/features/inventory/components/item/product-inventory-widget-exposed.tsx
+++ src/features/inventory/components/item/product-inventory-widget-exposed.tsx
@@ -2,36 +2,56 @@ import { EntryLinkProvider, PlatformProvider } from "@khinemyaezin/seller-ui";
 import { type HateoasLink } from "@khinemyaezin/seller-api";
 import {
   InventoryCreateContext,
+  InventoryPayload,
   PRODUCT_EXTENSION_SLOTS,
   type ExtensionMountProps,
   type SellerPlatform,
 } from "@khinemyaezin/seller-contracts";
 import ProductInventoryWidget from "./product-inventory-widget";
-import useInventoryNewSlot from "../../hooks/use-inventory-new-slot";
+import useInventoryNewSlot, {
+  type UseInventoryNewSlotProps,
+} from "../../hooks/use-inventory-new-slot";
 
 export type ProductInventoryWidgetExposedProps = ExtensionMountProps & {
   entryLink: HateoasLink;
   platform?: SellerPlatform;
 };
 
+function ProductInventoryWidgetBound(props: UseInventoryNewSlotProps) {
+  const { context, payload, ref, onChange } = useInventoryNewSlot(props);
+
+  return (
+    <ProductInventoryWidget
+      ref={ref}
+      context={context}
+      value={payload}
+      onChange={onChange}
+    />
+  );
+}
+
 export default function ProductInventoryWidgetExposed({
   groupId,
   slotId = PRODUCT_EXTENSION_SLOTS.CREATE_INVENTORY,
-  context: initialContext,
+  context,
+  initialValue,
+  onChange,
+  registerHandle,
   platform,
   entryLink,
 }: ProductInventoryWidgetExposedProps) {
-  const { context, payload, ref, onChange } = useInventoryNewSlot({ groupId, slotId, platform, initialContext: initialContext as InventoryCreateContext });
   if (!entryLink || !groupId) return null;
 
   return (
     <PlatformProvider platform={platform}>
       <EntryLinkProvider link={entryLink}>
-        <ProductInventoryWidget
-          ref={ref}
-          context={context}
-          value={payload}
-          onChange={onChange}
+        <ProductInventoryWidgetBound
+          groupId={groupId}
+          slotId={slotId}
+          context={context as InventoryCreateContext | undefined}
+          initialValue={initialValue as InventoryPayload | undefined}
+          onChange={onChange as ((value: InventoryPayload) => void) | undefined}
+          registerHandle={registerHandle}
         />
       </EntryLinkProvider>
     </PlatformProvider>
